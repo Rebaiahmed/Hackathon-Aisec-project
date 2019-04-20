b0fe74a56a531f038b945269a3638543a2247082
Ahmed <rebai.ahmed@outlook.com>
2019-04-20 12:42:58 +0100

    Updates

---
+++ precache-manifest.8b78757cf4b1127a2c5c02c13de16f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackathonmap/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c2715b86f8631cfadf43",
+    "url": "/hackathonmap/static/js/runtime~main.c2715b86.js"
+  },
+  {
+    "revision": "85fb22260d852a176b14",
+    "url": "/hackathonmap/static/js/main.85fb2226.chunk.js"
+  },
+  {
+    "revision": "8ffc381147906c085c0b",
+    "url": "/hackathonmap/static/js/1.8ffc3811.chunk.js"
+  },
+  {
+    "revision": "85fb22260d852a176b14",
+    "url": "/hackathonmap/static/css/main.02e8e98a.chunk.css"
+  },
+  {
+    "revision": "8ffc381147906c085c0b",
+    "url": "/hackathonmap/static/css/1.5533d40c.chunk.css"
+  },
+  {
+    "revision": "9262c9471486ce3ec6999bf48a31efc5",
+    "url": "/hackathonmap/index.html"
+  }
+];
